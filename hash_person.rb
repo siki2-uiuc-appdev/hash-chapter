3c8b29cd6d35972fe2976e8890dcab2fc59dc7ab
siki2-uiuc <siki2@illinois.edu>
2022-09-28 17:38:43 +0000

    chapter progress

--- hash_person.rb
+++ hash_person.rb
@@ -2,3 +2,9 @@
 # Make sure the value of the :age key is an Integer
 
 p "Enter a name, age, and occupation separated by spaces:"
+
+user = gets.chomp.split(' ')
+
+hash_person = { :name => user[0], :age => user[1].to_i, :occupation => user[2]}
+
+p hash_person
